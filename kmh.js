8d1c0c0ca11a44e54a2091ca622b409aa637452c
hhse <36848692+hhse@users.noreply.github.com>
2022-03-14 14:16:09 +0800

    Update kmh.js

--- kmh.js
+++ kmh.js
@@ -7,11 +7,10 @@ app下载地址：商店搜索：看漫
 群1077223830
 *******************************
 [rewrite_local]
-^https?:\/\/api-cdn\.321mh\.com\/comic-api\/v2  url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/kmh.js
+^https?:\/\/api-cdn\.321mh\.com\/comic-api\/v2 url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/kmh.js
 [mitm] 
 hostname = *.*.*
 
 *******************************/
 
-
-var body=$response['body'];var url=$request['url'];var obj=JSON['parse'](body);const vip='/comic/getcomicmaindata';if(url['indexOf'](vip)!=-0x1){body=replace(/price":((\d)+)/ig,body,'price\x22:0');body=replace(/isbuy":((\d)+)/ig,body,'isbuy\x22:0');}function replace(_0x985f68,_0x5e4e3b,_0x1e34ae){return _0x5e4e3b['replace'](_0x985f68,function(_0x433e51){return _0x1e34ae;});}$done({'body':body});
+var body=$response['body'];var url=$request['url'];var obj=JSON['parse'](body);const vip='/comic/getcomicmaindata';if(url['indexOf'](vip)!=-0x1){body=replace(/price":((\d)+)/ig,body,'price\x22:0');body=replace(/isbuy":((\d)+)/ig,body,'isbuy\x22:0');}function replace(_0x1dc1fd,_0x1a2e05,_0x41aa7e){return _0x1a2e05['replace'](_0x1dc1fd,function(_0xb8120e){return _0x41aa7e;});}$done({'body':body});
