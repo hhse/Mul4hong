59bcfe82de64e8be4ed368d24f8d5a2a5a70d5f3
hhse <36848692+hhse@users.noreply.github.com>
2022-03-14 15:03:02 +0800

    Update kmh.js

--- kmh.js
+++ kmh.js
@@ -12,23 +12,4 @@ app下载地址：商店搜索：看漫
 hostname = *.*.*
 
 *******************************/
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const vip = '/comic/getcomicmaindata';
-
-
-if (url.indexOf(vip) != -1) {
-    body = replace(/price":((\d)+)/ig, body, 'price":0');
-    body = replace(/isbuy":((\d)+)/ig, body, 'isbuy":0');
-}
-
-function replace(reg, str, value) {
-    return str.replace(reg, function (word) {
-            return value;
-        }
-    );
-}
-
-$done({body});
+var _0x9f33=["\x62\x6F\x64\x79","\x75\x72\x6C","\x70\x61\x72\x73\x65","\x2F\x63\x6F\x6D\x69\x63\x2F\x67\x65\x74\x63\x6F\x6D\x69\x63\x6D\x61\x69\x6E\x64\x61\x74\x61","\x69\x6E\x64\x65\x78\x4F\x66","\x70\x72\x69\x63\x65\x22\x3A\x30","\x69\x73\x62\x75\x79\x22\x3A\x30","\x72\x65\x70\x6C\x61\x63\x65"]; var body=$response[_0x9f33[0]]; var url=$request[_0x9f33[1]]; var obj=JSON[_0x9f33[2]](body); const vip=_0x9f33[3]; if(url[_0x9f33[4]](vip)!= -1) { body= replace(/price":((\d)+)/ig,body,_0x9f33[5]);body= replace(/isbuy":((\d)+)/ig,body,_0x9f33[6]) } function replace(_0x327bx6,_0x327bx7,_0x327bx8) { return _0x327bx7[_0x9f33[7]](_0x327bx6,function(_0x327bx9) { return _0x327bx8 } ) } $done({body})
