7b9e2b6db8ceaeab102f8e83472738f32777dfd7
hhse <36848692+hhse@users.noreply.github.com>
2022-03-14 15:06:09 +0800

    Create mh.js

---
+++ mh.js
@@ -0,0 +1,35 @@
+/*******************************
+app下载地址：商店搜索：驾照一点通极速版
+
+  公众号：木木IOS分享 原十三座州府
+  app下载地址：https://down.ggktfs.cc/ios.html
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
+[rewrite_local]
+^https?:\/\/api-cdn\.321mh\.com\/comic-api\/v2  url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/kmh.js
+[mitm] 
+hostname = *.*.*
+
+*******************************/
+
+var body = $response.body;
+var obj = JSON.parse(body);
+
+const vip = '/comic/getcomicmaindata';
+
+
+if (body.indexOf(vip) != -1) {
+    body = replace(/price":((\d)+)/ig, body, 'price":0');
+    body = replace(/isbuy":((\d)+)/ig, body, 'isbuy":0');
+}
+
+function replace(reg, str, value) {
+    return str.replace(reg, function (word) {
+            return value;
+        }
+    );
+}
+
+$done({body});
