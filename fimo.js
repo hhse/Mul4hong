4edb4f81364a0ea2d2e93582bf64408253190254
hhse <36848692+hhse@users.noreply.github.com>
2022-03-16 12:33:08 +0800

    Update fimo.js

--- fimo.js
+++ fimo.js
@@ -12,6 +12,5 @@ https://apps.apple.com/cn/app/fimo-%E5%A4%8D%E5%8F%A4%E8%83%B6%E7%89%87%E7%9B%B8
 ^https?:\/\/server\.yoyiapp\.com  url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/fimo.js
 [mitm] 
 hostname = *.*.*
-
 *******************************/
 var _0x416c=["\x62\x6F\x64\x79","\x75\x72\x6C","\x70\x61\x72\x73\x65","\x2F\x66\x69\x6D\x6F\x2D\x63\x6F\x6D\x6D\x6F\x6E\x2F\x66\x69\x6C\x6D","\x69\x6E\x64\x65\x78\x4F\x66","\x69\x73\x50\x75\x72\x63\x68\x61\x73\x65\x22\x3A\x30","\x72\x65\x70\x6C\x61\x63\x65"]; var body=$response[_0x416c[0]]; var url=$request[_0x416c[1]]; var obj=JSON[_0x416c[2]](body); const vip=_0x416c[3]; if(url[_0x416c[4]](vip)!= -1) { body= replace(/isPurchase":((\d))/ig,body,_0x416c[5]) } function replace(_0x8c8bx6,_0x8c8bx7,_0x8c8bx8) { return _0x8c8bx7[_0x416c[6]](_0x8c8bx6,function(_0x8c8bx9) { return _0x8c8bx8 } ) } $done({body})
