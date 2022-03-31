43ea3d793730052baef5f10e5d42f3dccc087dab
hhse <36848692+hhse@users.noreply.github.com>
2022-03-31 10:40:54 +0800

    Create ssa.js

---
+++ ssa.js
@@ -0,0 +1,20 @@
+[rewrite_local]
+^http[s]s:\/\/www\.ssalegs\.store url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/wf.js
+[mitm] 
+hostname = www.ssalegs.store
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/cms/Appapi/username/username';
+
+if (url.indexOf(vip) != -1) {
+    obj.point= 999999;
+    obj.jomkenylv = 99;
+    obj.groupname = "白金会员";
+    obj.overduedate = 2027383523;
+    obj.overduedate2 = 2027383523;
+    body = JSON.stringify(obj);
+}
+$done({body});
