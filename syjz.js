9a973e875e1457adce89bc626778508b624618c4
hhse <36848692+hhse@users.noreply.github.com>
2022-04-11 11:47:21 +0800

    Update syjz.js

--- syjz.js
+++ syjz.js
@@ -10,11 +10,11 @@ hostname = api.shayujizhang.com
 
 QuanX1.0.10：
 [rewrite_local]
-https:\/\/api\.shayujizhang\.com\/account\/grant\/detail\/info url script-response-body https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/shyjzh.js
+https:\/\/api\.shayujizhang\.com\/account\/grant\/detail\/info url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/syjz.js
 
 Surge4:
 [Script]
-http-response https:\/\/api\.shayujizhang\.com\/account\/grant\/detail\/info requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/shyjzh.js
+http-response https:\/\/api\.shayujizhang\.com\/account\/grant\/detail\/info requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/hhse/Mul4hong/master/syjz.js
 
 
 [MITM]
