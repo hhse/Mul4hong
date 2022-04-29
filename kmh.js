b065df367eb4f5cbdadb0808c59b96aa63b88b4c
hhse <36848692+hhse@users.noreply.github.com>
2022-04-29 16:58:09 +0800

    Update kmh.js

--- kmh.js
+++ kmh.js
@@ -1,15 +1,36 @@
 /*******************************
-app下载地址：商店搜索：看漫
+app下载地址：商店搜索：驾照一点通极速版
 
   公众号：木木IOS分享 原十三座州府
-使用声明：️此脚本仅供学习与交流，
-        请勿转载与贩卖！️️️
+  app下载地址：https://down.ggktfs.cc/ios.html
+使用声明：此脚本仅供学习与交流，
+        请勿转载与贩卖！
 群1077223830
 *******************************
 [rewrite_local]
-^http[s]?:\/\/api-cdn\.321mh\.com\/comic-api\/v2 url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/kmh.js
+^https?:\/\/api-cdn\.321mh\.com\/comic-api\/v2  url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/kmh.js
 [mitm] 
 hostname = *.*.*
 
 *******************************/
-var _0x9f33=["\x62\x6F\x64\x79","\x75\x72\x6C","\x70\x61\x72\x73\x65","\x2F\x63\x6F\x6D\x69\x63\x2F\x67\x65\x74\x63\x6F\x6D\x69\x63\x6D\x61\x69\x6E\x64\x61\x74\x61","\x69\x6E\x64\x65\x78\x4F\x66","\x70\x72\x69\x63\x65\x22\x3A\x30","\x69\x73\x62\x75\x79\x22\x3A\x30","\x72\x65\x70\x6C\x61\x63\x65"]; var body=$response[_0x9f33[0]]; var url=$request[_0x9f33[1]]; var obj=JSON[_0x9f33[2]](body); const vip=_0x9f33[3]; if(url[_0x9f33[4]](vip)!= -1) { body= replace(/price":((\d)+)/ig,body,_0x9f33[5]);body= replace(/isbuy":((\d)+)/ig,body,_0x9f33[6]) } function replace(_0x327bx6,_0x327bx7,_0x327bx8) { return _0x327bx7[_0x9f33[7]](_0x327bx6,function(_0x327bx9) { return _0x327bx8 } ) } $done({body})
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/comic/getcomicmaindata';
+
+
+if (url.indexOf(vip) != -1) {
+    body = replace(/price":((\d)+)/ig, body, 'price":0');
+    body = replace(/isbuy":((\d)+)/ig, body, 'isbuy":0');
+}
+
+function replace(reg, str, value) {
+    return str.replace(reg, function (word) {
+            return value;
+        }
+    );
+}
+
+$done({body});
