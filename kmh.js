55973fdd4495c4017df0d8bb461267960bb8b6d5
hhse <36848692+hhse@users.noreply.github.com>
2022-05-17 12:13:56 +0800

    Update kmh.js

--- kmh.js
+++ kmh.js
@@ -14,25 +14,4 @@ hostname = comic.321mh.com
 
 *******************************/
 
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const vip = '/comic/getcomicmaindata';
-
-
-if (url.indexOf(vip) != -1) {
-    body = replace(/price":((\d)+)/ig, body, 'price":0');
-    body = replace(/isbuy":((\d)+)/ig, body, 'isbuy":0');
-}
-
-function replace(reg, str, value) {
-    return str.replace(reg, function (word) {
-            return value;
-        }
-    );
-}
-
-$done({body});
-
-
+var body=$response['body'];var url=$request['url'];var obj=JSON['parse'](body);const vip='/comic/getcomicmaindata';if(url['indexOf'](vip)!=-(0x4a206^0x4a207)){body=replace(/price":((\d)+)/ig,body,'price\x22:0');body=replace(/isbuy":((\d)+)/ig,body,'isbuy\x22:0');}function replace(_0xa12671,_0x5ed061,_0x44d4b9){return _0x5ed061['replace'](_0xa12671,function(_0x35982f){return _0x44d4b9;});}$done({'body':body});
