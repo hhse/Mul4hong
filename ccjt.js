eaa4ff2fd7b2ae144a1dc6667081a3a925c093da
hhse <36848692+hhse@users.noreply.github.com>
2022-05-31 13:25:42 +0800

    Create ccjt.js

---
+++ ccjt.js
@@ -0,0 +1,34 @@
+/*******************************
+
+[rewrite_local]
+^http:\/\/cc\.lzjoy\.com\/.?urlparam.+$  url script-response-body ccjt.js
+[mitm] 
+hostname = *.*.*
+
+*******************************/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/GetVip';
+const pay = '/detail';
+
+
+if (url.indexOf(vip) != -1) {
+  obj.list.is_vip = "1";
+  obj.list.expire_date = "2030-11-12";
+      body = JSON.stringify(obj);
+}
+if (url.indexOf(pay) != -1) {
+  obj.list.has_buy = "1";
+      body = JSON.stringify(obj);
+}
+
+$done({body});
+
+
+
+
+  
+
