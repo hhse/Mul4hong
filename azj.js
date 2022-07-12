51ee2c4e24940ef670255d8fe65c7360c61e8aef
hhse <36848692+hhse@users.noreply.github.com>
2022-07-12 21:09:43 +0800

    Create azj.js

---
+++ azj.js
@@ -0,0 +1,22 @@
+/*
+app下载地址：商店搜索：❤️证件
+[rewrite_local]
+^https:\/\/zhengjian\.flyingeffect\.com\/api\/ url script-response-body azj.js
+[mitm] 
+hostname = zhengjian.flyingeffect.com
+公众号：木木IOS分享 原十三座州府
+群1077223830
+*/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/user/userInfo';
+
+if (url.indexOf(vip) != -1) {
+    obj.data.is_vip = 1;
+    obj.data.vip_end_time = "2030-08-10 18:14:09";
+    body = JSON.stringify(obj);
+}
+$done({body});
