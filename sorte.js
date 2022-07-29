bc7e190b4370923ed6b7c02d166957959e7a528b
hhse <36848692+hhse@users.noreply.github.com>
2022-07-29 13:15:15 +0800

    Create sorte.js

---
+++ sorte.js
@@ -0,0 +1,33 @@
+/*******************************
+
+  公众号：木木IOS分享 原十三座州府
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
+[rewrite_local]
+^https:\/\/api\.sortedapp\.com\/receipts url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/sorte.js
+[mitm] 
+hostname = api.sortedapp.com
+
+*******************************/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/verify';
+
+
+if (url.indexOf(vip) != -1) {
+    obj.isPro = true;
+}
+
+function replace(reg, str, value) {
+    return str.replace(reg, function (word) {
+            return value;
+        }
+    );
+}
+
+$done({body});
