36f5de7a66701b82d4441fa4b27e4d724931b90c
hhse <36848692+hhse@users.noreply.github.com>
2022-08-30 21:52:14 +0800

    Update mumu.js

--- mumu.js
+++ mumu.js
@@ -1,5 +1,10 @@
+/*******************************
 
-/*
+  公众号：木木IOS分享 原十三座州府
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
 [rewrite_local]
 ^https:\/\/uu\.tuanyougou\.com\/\/video url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/mumu.js
 [mitm] 
@@ -7,13 +12,23 @@ hostname = uu.tuanyougou.com
 
 //hostname :uu.tuanyougou.com
 //重写链接：^https:\/\/uu\.tuanyougou\.com\/\/video\/registryUser
-*/ 
+
+*******************************/
+
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
-if (url.indexOf('/registryUser') != -1){
-    obj.data.endTime = "木木大帅比";
-    
+
+const mumu = '/registryUser';
+
+
+if (url.indexOf(mumu) != -1) {
+    obj.data.isVip = 1;
+obj.data.endTime = "木木讲完啦";
+
+
+	body = JSON.stringify(obj);
 }
 
-$done({body: JSON.stringify(obj)});
+
+$done({body});
