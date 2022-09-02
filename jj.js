5452382e6dd3c609b94695b933cd4ebe284d7bd8
hhse <36848692+hhse@users.noreply.github.com>
2022-09-02 19:19:02 +0800

    Create jj.js

---
+++ jj.js
@@ -0,0 +1,26 @@
+
+// [rewrite_local]
+// ^https?:\/\/server\.yoyiapp\.com  url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/fimo.js
+// [mitm] 
+// hostname = *.*.*
+
+
+
+var body = $response.body;
+var obj = JSON.parse(body);
+
+const vip = '/fimo-common/film';
+
+
+if (body.indexOf(vip) != -1) {
+    body = replace(/isPurchase":((\d)+)/ig, body, 'isPurchase":0');
+}
+
+function replace(reg, str, value) {
+    return str.replace(reg, function (word) {
+            return value;
+        }
+    );
+}
+
+$done({body});
