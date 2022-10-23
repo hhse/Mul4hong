0beade293bac98462c800aa31e1c267dd506ad4c
hhse <36848692+hhse@users.noreply.github.com>
2022-10-23 15:52:29 +0800

    Update Ceshi.js

--- Ceshi.js
+++ Ceshi.js
@@ -1,16 +1,20 @@
-/*
-公众号:木木IOS分享
+/*******************************
+  公众号:木木IOS分享
 关注了解更多新科技！！！
 木木测试脚本
 脚本名称:木木测试脚本
-
-
-[rewrite_local]  
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
+[rewrite_local]
 ^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/Ceshi.js
 [mitm] 
-*.tuanyougou.*
-*/
+hostname = *.tuanyougou.*
+
+*******************************/
 var obj = JSON.parse($response.body);
     obj.data.userId = 9999;
 obj.data.endTime = "脚本成功啦";
+obj.data.vip \d+ = 1;
     $done({body: JSON.stringify(obj)});
