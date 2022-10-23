c8729b91217b683ffa5683635e3f53fd69280350
hhse <36848692+hhse@users.noreply.github.com>
2022-10-23 15:39:00 +0800

    Update Ceshi.js

--- Ceshi.js
+++ Ceshi.js
@@ -4,10 +4,11 @@
 木木测试脚本
 脚本名称:木木测试脚本
 
-[mitm] 
-*.tuanyougou.*
+
 [rewrite_local]  
 ^http[s]?:\/\/uu.tuanyougou.com\/\/video\/registryUser.+$ url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/Ceshi.js
+[mitm] 
+*.tuanyougou.*
 */
 var obj = JSON.parse($response.body);
     obj.data.userId = 9999;
